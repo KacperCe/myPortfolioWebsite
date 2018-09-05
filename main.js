b0d00bd7e81ee3cfc7b23b4353840daf4aa7a79b
kacper_ce <kacpercepinski@gmail.com>
2018-09-05 18:58:01 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', function() {
+    var currScrollPos2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (currScrollPos2 > 100) {
+      document.getElementById('hero').style.opacity = -currScrollPos2 / 120 + 2;
+    }
+  }
+);
