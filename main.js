63a21fbb0c395b291973ddce0d28ed57ae47492c
KacperCe <kacpercepinski@gmail.com>
2018-09-06 22:25:42 +0200

    animated arrow scroll opacity

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.addEventListener('scroll', function() {
     var currScrollPos2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     if (currScrollPos2 > 100) {
       document.getElementById('hero').style.opacity = -currScrollPos2 / 120 + 2;
+      document.getElementById('arrow-down').style.opacity = -currScrollPos2 / 120 + 2;
     }
   }
 );
