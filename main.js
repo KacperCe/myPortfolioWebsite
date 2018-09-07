2dabb0b47c7b5a8b03c2c15dd92cb9e115c24db3
KacperCe <kacpercepinski@gmail.com>
2018-09-07 21:57:23 +0200

    menu styling

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 window.addEventListener('scroll', function() {
-    var currScrollPos2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (currScrollPos2 > 100) {
-      document.getElementById('hero').style.opacity = -currScrollPos2 / 120 + 2;
-      document.getElementById('arrow-down').style.opacity = -currScrollPos2 / 120 + 2;
+    let currScrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (currScrollPos > 100) {
+      document.getElementById('hero').style.opacity = -currScrollPos / 120 + 2;
+      document.getElementById('arrow-down').style.opacity = -currScrollPos / 120 + 2;
+      
     }
+    
+      
+      
+    
   }
 );
 
@@ -11,16 +16,16 @@ let toggleNavStatus = false;
 
 let toggleNav = function () {
     let getButton = document.querySelector(".btn-toggle-nav");
-    let getTopBar = document.querySelector(".nav-main");
-    let getTopBarUl = document.querySelector(".nav-main ul");
-    let getTopBarLinks = document.querySelectorAll(".nav-main a");
+    let getTopBar = document.querySelector("#nav-main");
+    let getTopBarUl = document.querySelector("#nav-main ul");
+    let getTopBarLinks = document.querySelectorAll("#nav-main a");
 
 
     if (toggleNavStatus == false) {
         getButton.classList.toggle("btn-close");
         getTopBarUl.style.visibility = "visible";
-        getTopBar.style.height = "60vh";
-        getTopBar.style.background = "rgba(255, 255, 255, .9)";
+        getTopBar.style.height = "70vh";
+        getTopBar.style.background = "rgba(255, 255, 255, .95)";
 
         let arrayLength = getTopBarLinks.length;
         for (let index = 0; index < arrayLength; index++) {
